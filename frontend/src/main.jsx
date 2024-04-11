a2da8bb18981ff60e0a317d66cb8a30d6a7dab5d
shareef <shareefsk219@gmail.com>
2024-04-11 21:30:22 +0530

    integration also completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthContextProvider } from './context/AuthContext.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
+    <AuthContextProvider>
     <App />
-  </React.StrictMode>,
-)
+    </AuthContextProvider>  
+    </BrowserRouter>
+  </React.StrictMode>
+);
