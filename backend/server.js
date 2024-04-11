1e5d2a9c1ce37c3daf6d8387abb8614057f48e47
shareef <shareefsk219@gmail.com>
2024-04-11 23:00:37 +0530

    socket also completed

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,9 @@ import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
 import connectToMongoDB from './db/connectToMongoDB.js';
 import cookieParser from 'cookie-parser'
+import { app, server } from './socket/socket.js';
 
-const app = express();
+
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 
@@ -23,7 +24,7 @@ app.get("/", (req, res) => {
 
 
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectToMongoDB()
     console.log(`server is running on port ${PORT}`);
 });
